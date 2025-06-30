7295391a5207f7ae29f7f0d786866ef7abf6e2b6
sqizzo <baihaqimnb@gmail.com>
2025-06-30 14:52:15 +0700

    finishing up

--- data/students.js
+++ data/students.js
@@ -2,27 +2,27 @@ var students = [
   {
     id: "12345",
     name: "Septian Dwi Cahyo",
-    grade: 100,
+    grade: 85,
   },
   {
     id: "12932",
     name: "Muhammad Riyansyah Afandi",
-    grade: 100,
+    grade: 95,
   },
   {
     id: "19023",
     name: "Rasya Radja",
-    grade: 100,
+    grade: 89,
   },
   {
     id: "18902",
-    name: "Muhammad Daffa Ananda",
-    grade: 100,
+    name: "Muhammad Daffa Ananda Budi",
+    grade: 92,
   },
   {
     id: "11320",
     name: "Muhamad Nur Baihaqi",
-    grade: 100,
+    grade: 70,
   },
 ];
 
--- routes/students.js
+++ routes/students.js
@@ -4,7 +4,22 @@ const students = require("../data/students");
 const { customAlphabet } = require("nanoid");
 
 router.get("/", (req, res, next) => {
-  res.send(students);
+  const minGrade = req.query.minGrade;
+  const sort = req.query.sort;
+
+  let result = students;
+
+  if (minGrade !== undefined) {
+    result = result.filter((student) => student.grade >= Number(minGrade));
+  }
+
+  if (sort === "name") {
+    result.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (sort === "grade") {
+    result.sort((a, b) => b.grade - a.grade);
+  }
+
+  res.send(result);
 });
 
 router.get("/:id", (req, res, next) => {
@@ -17,6 +32,27 @@ router.get("/:id", (req, res, next) => {
   }
 
   res.send(filteredData);
+});
+
+router.put("/:id", (req, res, next) => {
+  const id = req.params.id;
+
+  const studentIndex = students.findIndex((student) => student.id === id);
+  const { name, grade } = req.body;
+
+  if (studentIndex === -1) {
+    res.status(404).json({ error: "Student was not found" });
+  } else {
+    if (name) {
+      students[studentIndex].name = name;
+    }
+
+    if (grade !== undefined) {
+      students[studentIndex].grade = grade;
+    }
+
+    res.status(200).json(students[studentIndex]);
+  }
 });
 
 router.post("/", (req, res, next) => {
@@ -35,7 +71,20 @@ router.post("/", (req, res, next) => {
 
   students.push(newStudent);
 
-  res.status(201).json(newStudent);
+  res.status(201).json(students);
+});
+
+router.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
+
+  const studentIndex = students.findIndex((student) => student.id === id);
+
+  if (studentIndex === -1) {
+    res.status(404).json({ error: "Student was not found" });
+  }
+
+  students.splice(studentIndex, 1);
+  res.status(201).json(students);
 });
 
 module.exports = router;
